25310d54a7e34f55bcc615b12fc1ce8f8b90301b
SandeepThadiparthi <sandeepthadiparthi@gmail.com>
2023-07-20 23:32:13 +0530

    add hash function

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine','ejs');
 app.use(express.static("public"));
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 
 mongoose.connect("mongodb+srv://sandeepthadiparthi:Sandeep%401@cluster0.gjiu1s8.mongodb.net/userDB");
@@ -15,8 +15,6 @@ const userSchema = new mongoose.Schema({
     username:String,
     password:String
 });
-const secret = process.env.SECRET;
-userSchema.plugin(encrypt, { secret : secret, encryptedFields: ['password'] });
 
 
 
@@ -38,7 +36,7 @@ app.get("/register",function(req,res){
 app.post("/register",function(req,res){
     const newUser = new User ({
         username:req.body.username,
-        password:req.body.password
+        password:md5(req.body.password)
     });
     newUser.save().then(() => {
         res.render("secrets");
@@ -50,7 +48,7 @@ app.post("/register",function(req,res){
 
 app.post("/login",function(req,res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
     User.findOne({username:username}).then( (founduser) => {
         if (!founduser){
             console.log("no user found");
